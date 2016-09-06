5e44f1e4e077b3aba270e46564be771f4052394e
Noanon <noanon99@mail.ru>
2016-09-06 20:00:48 +0300

    dima kek

--- NFW/NFW/GlobalSettings.cs
+++ NFW/NFW/GlobalSettings.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//klass prinadlegit Dmitriyu
 namespace NFW
 {
     class GlobalSettings
